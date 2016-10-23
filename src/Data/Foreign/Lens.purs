92ebc7c79063f0b69963c8f1166ec3a7cb53ac21
Phil Freeman <paf31@cantab.net>
2016-10-22 21:55:55 -0700

    updates for 0.10

--- src/Data/Foreign/Lens.purs
+++ src/Data/Foreign/Lens.purs
@@ -14,48 +14,49 @@ module Data.Foreign.Lens
   ) where
 
 import Prelude
+import Control.Monad.Except (runExcept)
 import Data.Foreign (Foreign, readArray, readInt, readNumber, readBoolean, readChar, readString, parseJSON)
 import Data.Foreign.Index (index, prop) as F
 import Data.Foreign.Keys (keys) as F
-import Data.Lens (FoldP, traversed, to)
+import Data.Lens (Fold', traversed, to)
 import Data.Monoid (class Monoid)
 
 -- | A `Fold` which parses JSON.
-json :: forall r. Monoid r => FoldP r String Foreign
-json = to parseJSON <<< traversed
+json :: forall r. Monoid r => Fold' r String Foreign
+json = to (runExcept <<< parseJSON) <<< traversed
 
 -- | A `Fold` which reads a `String`.
-string :: forall r. Monoid r => FoldP r Foreign String
-string = to readString <<< traversed
+string :: forall r. Monoid r => Fold' r Foreign String
+string = to (runExcept <<< readString) <<< traversed
 
 -- | A `Fold` which reads a `Char`.
-char :: forall r. Monoid r => FoldP r Foreign Char
-char = to readChar <<< traversed
+char :: forall r. Monoid r => Fold' r Foreign Char
+char = to (runExcept <<< readChar) <<< traversed
 
 -- | A `Fold` which reads a `Boolean`.
-boolean :: forall r. Monoid r => FoldP r Foreign Boolean
-boolean = to readBoolean <<< traversed
+boolean :: forall r. Monoid r => Fold' r Foreign Boolean
+boolean = to (runExcept <<< readBoolean) <<< traversed
 
 -- | A `Fold` which reads a `Number`.
-number :: forall r. Monoid r => FoldP r Foreign Number
-number = to readNumber <<< traversed
+number :: forall r. Monoid r => Fold' r Foreign Number
+number = to (runExcept <<< readNumber) <<< traversed
 
 -- | A `Fold` which reads an `Int`.
-int :: forall r. Monoid r => FoldP r Foreign Int
-int = to readInt <<< traversed
+int :: forall r. Monoid r => Fold' r Foreign Int
+int = to (runExcept <<< readInt) <<< traversed
 
 -- | A `Fold` which reads an `Array`.
-array :: forall r. Monoid r => FoldP r Foreign (Array Foreign)
-array = to readArray <<< traversed
+array :: forall r. Monoid r => Fold' r Foreign (Array Foreign)
+array = to (runExcept <<< readArray) <<< traversed
 
 -- | A `Fold` which reads an object property.
-prop :: forall r. Monoid r => String -> FoldP r Foreign Foreign
-prop p = to (F.prop p) <<< traversed
+prop :: forall r. Monoid r => String -> Fold' r Foreign Foreign
+prop p = to (runExcept <<< F.prop p) <<< traversed
 
 -- | A `Fold` which reads an array index.
-index :: forall r. Monoid r => Int -> FoldP r Foreign Foreign
-index i = to (F.index i) <<< traversed
+index :: forall r. Monoid r => Int -> Fold' r Foreign Foreign
+index i = to (runExcept <<< F.index i) <<< traversed
 
 -- | A `Fold` which reads object keys.
-keys :: forall r. Monoid r => FoldP r Foreign (Array String)
-keys = to F.keys <<< traversed
+keys :: forall r. Monoid r => Fold' r Foreign (Array String)
+keys = to (runExcept <<< F.keys) <<< traversed
--- test/Main.purs
+++ test/Main.purs
@@ -1,20 +1,18 @@
 module Test.Main where
 
 import Prelude
-
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
-import Data.Foldable (traverse_)
 import Data.Foreign (Foreign, toForeign)
 import Data.Foreign.Lens (string, prop, array)
-import Data.Lens (FoldP(), (^..), traversed)
+import Data.Lens (Fold', sequenceOf_, traversed, to)
 import Data.Monoid (class Monoid)
 
 doc :: Foreign
 doc = toForeign { paras: [ { word: "Hello" }, { word: "World" } ] }
 
--- | This `FoldP` extracts all words appearing in a structure like the one above.
-words :: forall r. Monoid r => FoldP r Foreign String
+-- | This `Fold'` extracts all words appearing in a structure like the one above.
+words :: forall r. Monoid r => Fold' r Foreign String
 words = prop "paras"
     <<< array
     <<< traversed
@@ -22,4 +20,4 @@ words = prop "paras"
     <<< string
 
 main :: forall e. Eff (console :: CONSOLE | e) Unit
-main = traverse_ log (doc ^.. words)
+main = sequenceOf_ (words <<< to log) doc
